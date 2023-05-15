3ec6389a7d6979e776a924cdf88c376656107c12
gilio <gilioligabriele2001@gmail.com>
2023-05-15 14:03:39 +0100

    Publish version 1.1.0

--- lib/src/node/marker_or_cluster_node.dart
+++ lib/src/node/marker_or_cluster_node.dart
@@ -9,5 +9,5 @@ abstract class MarkerOrClusterNode {
 
   MarkerOrClusterNode({required this.parent});
 
-  Bounds<double> pixelBounds(FlutterMapState map);
+  Bounds<num> pixelBounds(FlutterMapState map);
 }
